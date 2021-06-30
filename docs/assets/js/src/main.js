3c94710a543744647830969fe7043d8bdd18a94c
Mark Hanna <cipscis+github@gmail.com>
2021-06-30 13:06:38 +1200

    v1.0.0 - Copied project from portfolio into its own repo

--- docs/assets/js/src/main.js
+++ docs/assets/js/src/main.js
@@ -1,8 +1,30 @@
-// Using import from '/filename', Webpack will
-// bundle files from outside the docs directory
-// even though it is the root for the server
-// both locally and on GitHub Pages
+import {
+	bind,
+	unbind,
 
-import { foo } from '/main.js';
+	bindSequence,
+	unbindSequence,
+} from '/keybinding.js';
 
-foo();
+const setMessage = (message) => {
+	return (e) => {
+		e.preventDefault();
+
+		const $el = document.querySelector('.js-text-message');
+		$el.textContent = message;
+	};
+};
+
+const konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a' ,'enter'];
+const cheat = setMessage('Hey, stop cheating!');
+
+bind('a', setMessage('You pressed the \'a\' key'));
+bind('m', setMessage('You pressed Ctrl + \'m\''), true, true);
+
+bindSequence(konamiCode, cheat);
+
+document.querySelector('.js-unbind-sequence').addEventListener('click', (e) => unbindSequence(konamiCode, cheat));
+
+const kPress = setMessage('You pressed the \'k\' key');
+document.querySelector('.js-bind').addEventListener('click', (e) => bind('k', kPress, true));
+document.querySelector('.js-unbind').addEventListener('click', (e) => unbind('k', kPress));
