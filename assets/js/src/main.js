9a590d09b02cfcd5a8e5286b273aabdf2df29ff3
Cipscis <cipscis+github@gmail.com>
2021-07-05 09:19:49 +0000

    Deploying to gh-pages from @ Cipscis/keybinding@162ece98d75a421c542e78ef0c74ceb2688574ad 🚀

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -8,23 +8,21 @@ import {
 
 const setMessage = (message) => {
 	return (e) => {
-		e.preventDefault();
-
 		const $el = document.querySelector('.js-text-message');
 		$el.textContent = message;
 	};
 };
 
-const konamiCode = ['arrowup', 'arrowup', 'arrowdown', 'arrowdown', 'arrowleft', 'arrowright', 'arrowleft', 'arrowright', 'b', 'a' ,'enter'];
-const cheat = setMessage('Hey, stop cheating!');
-
 bind('a', setMessage('You pressed the \'a\' key'));
-bind('m', setMessage('You pressed Ctrl + \'m\''), true, true);
+bind('ctrl+m', setMessage('You pressed Ctrl + \'m\''), { allowInInput: true });
 
-bindSequence(konamiCode, cheat);
 
-document.querySelector('.js-unbind-sequence').addEventListener('click', (e) => unbindSequence(konamiCode, cheat));
+const konamiCode = 'up up down down left right left right b a enter';
+const cheat = setMessage('Hey, stop cheating!');
+bind(konamiCode, cheat);
+
+document.querySelector('.js-unbind-sequence').addEventListener('click', (e) => unbind(konamiCode, cheat));
 
 const kPress = setMessage('You pressed the \'k\' key');
-document.querySelector('.js-bind').addEventListener('click', (e) => bind('k', kPress, true));
+document.querySelector('.js-bind').addEventListener('click', (e) => bind('k', kPress, { allowInInput: true }));
 document.querySelector('.js-unbind').addEventListener('click', (e) => unbind('k', kPress));
